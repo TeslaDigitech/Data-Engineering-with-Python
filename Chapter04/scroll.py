a563925ac6ce16227945258b83efd997b03d9bb4
ArulArockiasamy <114662003+ArulArockiasamy@users.noreply.github.com>
2023-03-31 11:08:54 +0200

    Update scroll.py

--- Chapter04/scroll.py
+++ Chapter04/scroll.py
@@ -1,30 +1,38 @@
+# Import the Elasticsearch library
 from elasticsearch import Elasticsearch
 
+# Create an Elasticsearch instance
+es = Elasticsearch()
 
-
-es = Elasticsearch() 
-
-
+# Execute a search query on the Elasticsearch instance to fetch the first batch of documents
 res = es.search(
-  index = 'users',
-  doc_type = 'doc',
-  scroll = '20m',
-  size = 500,
-  body = {"query":{"match_all":{}}}
+    index='users',
+    doc_type='doc',
+    scroll='20m',  # Keep the search context alive for 20 minutes
+    size=500,  # Fetch 500 documents per batch
+    body={"query": {"match_all": {}}}
 )
+
+# Print the '_source' field of the fetched documents
 for search_doc in res['hits']['hits']:
     print(search_doc['_source'])
+
+# Store the scroll ID and the total number of documents found
 sid = res['_scroll_id']
 size = res['hits']['total']['value']
 
-  # Start scrolling
+# Start scrolling through the remaining documents
 while (size > 0):
     print(size)
-    print("--------------------------------------------")   
-    res = es.scroll(scroll_id = sid, scroll = '20m')
+    print("--------------------------------------------")
 
+    # Fetch the next batch of documents using the scroll ID
+    res = es.scroll(scroll_id=sid, scroll='20m')
+
+    # Update the scroll ID and the number of documents in the current batch
     sid = res['_scroll_id']
     size = len(res['hits']['hits'])
 
+    # Print the '_source' field of the fetched documents in the current batch
     for doc in res['hits']['hits']:
         print(doc['_source'])
