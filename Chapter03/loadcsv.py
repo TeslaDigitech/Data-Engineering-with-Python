e7fa5ad8bf5ee255fd99ce6f6bbac3a34c4004c3
ArulArockiasamy <114662003+ArulArockiasamy@users.noreply.github.com>
2023-03-31 10:00:47 +0200

    Update loadcsv.py

--- Chapter03/loadcsv.py
+++ Chapter03/loadcsv.py
@@ -1,3 +1,22 @@
+# This Python code generates a CSV file named 'data.csv' with 1000 rows of fake data using the Faker library. The fake data includes information such as name, age, street address, city, state, zip code, longitude, and latitude. The code follows these steps:
+
+# Import necessary libraries (Faker and CSV)
+# Open 'data.csv' in write mode
+# Create an instance of the Faker class
+# Define the header for the CSV file
+# Create a CSV writer object to write data to the output file
+# Write the header row to the output file
+# Generate and write 1000 rows of fake data to the output file using a for loop
+# Close the output file
+# Here is the logical execution order in a table format:
+
+# Order	Method/Statement	Description
+# 1	from faker import Faker	Import the Faker library
+# 2	import csv	Import the CSV library
+# 3	output = open('data.csv', 'w')	Open 'data.csv' in write mode
+# 4	fake = Faker()	
+
+
 from faker import Faker
 import csv
 output=open('data.csv','w')
