5a0ed8d28fb8689c21ee6690320ab5fbcc67fda8
ArulArockiasamy <114662003+ArulArockiasamy@users.noreply.github.com>
2023-03-31 10:38:08 +0200

    Update elasticsearchbulk.py

--- Chapter04/elasticsearchbulk.py
+++ Chapter04/elasticsearchbulk.py
@@ -1,23 +1,43 @@
+# This Python code uses the Elasticsearch library to insert 998 documents with randomly generated data into an Elasticsearch index called 'users'. The code follows these steps:
+
+# Import the Elasticsearch library and helpers module
+# Import the Faker library for generating fake data
+# Create a Faker instance
+# Create an Elasticsearch client
+# Generate a list of actions (documents) to be inserted into Elasticsearch
+# Perform a bulk operation to insert the documents into Elasticsearch
+# Print the response from the Elasticsearch bulk operation
+
+# Import the Elasticsearch library
 from elasticsearch import Elasticsearch
+# Import the helpers module from the Elasticsearch library
 from elasticsearch import helpers
+# Import the Faker library for generating fake data
 from faker import Faker
 
-fake=Faker()
-es = Elasticsearch() #or pi {127.0.0.1}
+# Create a Faker instance
+fake = Faker()
 
+# Create an Elasticsearch client, connecting to the default address (localhost) and port (9200)
+es = Elasticsearch()
+
+# Generate a list of actions to be performed in bulk by Elasticsearch
 actions = [
   {
-    "_index": "users",
-    "_type": "doc",
-    "_source": {
-	"name": fake.name(),
-	"street": fake.street_address(), 
-	"city": fake.city(),
-	"zip":fake.zipcode()}
+    "_index": "users",            # Define the index name
+    "_type": "doc",               # Define the document type
+    "_source": {                  # Define the document source (the data to be inserted)
+        "name": fake.name(),      # Generate a random name
+        "street": fake.street_address(),  # Generate a random street address
+        "city": fake.city(),      # Generate a random city
+        "zip": fake.zipcode()     # Generate a random zip code
+    }
   }
-  for x in range(998) # or for i,r in df.iterrows()
+  for x in range(998)            # Repeat the process for 998 times (creating 998 documents)
 ]
 
+# Perform a bulk operation using the actions generated above
 response = helpers.bulk(es, actions)
-print(response)
 
+# Print the response from the Elasticsearch bulk operation
+print(response)
