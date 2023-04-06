c431fdb7d38c4fd070a274e616c21d04970826cb
ArulArockiasamy <114662003+ArulArockiasamy@users.noreply.github.com>
2023-04-06 14:20:53 +0200

    Update pythonlog.py

--- Chapter13/pythonlog.py
+++ Chapter13/pythonlog.py
@@ -1,20 +1,22 @@
+# Purpose:
+# The script demonstrates how to set up and use the logging module in Python. 
+# It shows two different configurations, one without a timestamp and one with a timestamp. 
+# The script logs various messages with different logging levels, such as debug, warning, error, and info.
 import logging
 
-logging.basicConfig(level=0,filename='python-log.log', filemode='w', format='%(levelname)s - %(message)s')
+# Set up the basic logging configuration with level, filename, filemode, and format
+logging.basicConfig(level=0, filename='python-log.log', filemode='w', format='%(levelname)s - %(message)s')
 
+# Log a debug message
 logging.debug('Attempted to divide by zero')
+# Log a warning message
 logging.warning('User left field blank in the form')
+# Log an error message
 logging.error("Couldn't find specified file")
 
-# > DEBUG - Attempted to divide by zero
-# > WARNING - User left field blank in the form
-# > ERROR - Couldn't find specified file
+# Reconfigure the logging setup to include a timestamp in the log format
+logging.basicConfig(level=0, filename='python-log.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-logging.basicConfig(level=0,filename='python-log.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+# Log two info messages with the new configuration
 logging.info('Something happened')
 logging.info('Something else happened, and it was bad')
-
-# > 2020-06-21 10:55:40,278 - INFO - Something happened
-# > 2020-06-21 10:55:40,278 - INFO - Something else happened, and it was bad
-
