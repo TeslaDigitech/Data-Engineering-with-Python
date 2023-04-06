0037fe1741bc5e25aeccdf39b6f6b25944fde6fb
ArulArockiasamy <114662003+ArulArockiasamy@users.noreply.github.com>
2023-04-06 13:48:33 +0200

    Update coords.py

--- Chapter06/coords.py
+++ Chapter06/coords.py
@@ -7,34 +7,47 @@ from java.nio.charset import StandardCharsets
 from org.apache.nifi.processor.io import StreamCallback
 from org.python.core.util import StringUtil
 
+# Define the ModJSON class that implements the StreamCallback interface
 class ModJSON(StreamCallback):
-  def __init__(self):
+    def __init__(self):
         pass
-  def process(self, inputStream, outputStream):
-    try:
-        text = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
-        reply=json.loads(text)
-        
-        reply['coords']=str(reply['lat'])+","+str(reply['lng'])
-        d=reply['created_at'].split("T")
-        reply['opendate']=d[0]
-        outputStream.write(bytearray(json.dumps(reply, indent=4).encode('utf-8')))
-    except:
-        global errorOccurred
-        errorOccurred=True
-        
-        outputStream.write(bytearray(json.dumps(reply, indent=4).encode('utf-8')))
-        
-errorOccurred=False
-flowFile = session.get()
-if (flowFile != None):
-  flowFile = session.write(flowFile, ModJSON())
-  #flowFile = session.putAttribute(flowFile)
-  if(errorOccurred):
-    session.transfer(flowFile, REL_FAILURE)
-  
-  else:
-    session.transfer(flowFile, REL_SUCCESS)
-  
-session.commit()
 
+    # Define the process method that takes inputStream and outputStream as arguments
+    def process(self, inputStream, outputStream):
+        try:
+            # Read the input stream and convert it to a string
+            text = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
+            # Parse the string to a JSON object
+            reply = json.loads(text)
+            
+            # Modify the JSON object by adding 'coords' and 'opendate' fields
+            reply['coords'] = str(reply['lat']) + "," + str(reply['lng'])
+            d = reply['created_at'].split("T")
+            reply['opendate'] = d[0]
+            
+            # Write the modified JSON object to the output stream
+            outputStream.write(bytearray(json.dumps(reply, indent=4).encode('utf-8')))
+        except:
+            global errorOccurred
+            errorOccurred = True
+            # Write the original JSON object to the output stream if an exception occurs
+            outputStream.write(bytearray(json.dumps(reply, indent=4).encode('utf-8')))
+        
+errorOccurred = False
+flowFile = session.get()
+
+# Check if the flowFile is not None
+if (flowFile != None):
+    # Write the flowFile using the ModJSON class
+    flowFile = session.write(flowFile, ModJSON())
+
+    # Check if an error occurred during processing
+    if(errorOccurred):
+        # Transfer the flowFile to the REL_FAILURE relationship
+        session.transfer(flowFile, REL_FAILURE)
+    else:
+        # Transfer the flowFile to the REL_SUCCESS relationship
+        session.transfer(flowFile, REL_SUCCESS)
+
+# Commit the NiFi session
+session.commit()
