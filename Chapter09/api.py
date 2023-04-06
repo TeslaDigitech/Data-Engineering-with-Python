e7d6f1b11038add61aac717be2d3013622201a21
ArulArockiasamy <114662003+ArulArockiasamy@users.noreply.github.com>
2023-04-06 14:05:15 +0200

    Update api.py

--- Chapter09/api.py
+++ Chapter09/api.py
@@ -1,9 +1,15 @@
-import requests
+# Purpose:
+# The script retrieves information from an Apache NiFi instance using its REST API.
+# It fetches data about system diagnostics, processor groups, processors, flowfile queues, bulletins, and counters.import requests
+
 
 #System Diagnostics
+# Retrieve system diagnostics
 r=requests.get('http://localhost:9300/nifi-api/system-diagnostics')
 data=r.json()
 print(data)
+
+# Access specific data from the system diagnostics
 data['systemDiagnostics']['aggregateSnapshot']['maxHeap']
 #'512 MB'
 data['systemDiagnostics']['aggregateSnapshot']['totalThreads']
@@ -12,6 +18,7 @@ data['systemDiagnostics']['aggregateSnapshot']['heapUtilization']
 #'81.0%'
 
 # Processor Group
+# Retrieve information about a processor group
 pg=requests.get('http://localhost:9300/nifi-api/process-groups/9466c3ca-4c6d-3884-ac72-af4a27919fb0')
 pgdata=pg.json()
 pgdata['component']['name']
@@ -20,13 +27,14 @@ pgdata['status']
 
 
 # A single processor
+# Retrieve information about a single processor
 p=requests.get('http://localhost:9300/nifi-api/processors/8b63e4d0-eff2-3093-f4ad-0f1581e56674')
 pdata=p.json()
 pdata['component']['name']
 #'Query SCF - Archive'
 pdata['status']
 
-# Queue Data
+# Retrieve queue data
 
 q=requests.post('http://localhost:9300/nifi-api/flowfile-queues/295fc119-0172-1000-3949-54311cdb478e/listing-requests')
 qdata=q.json()
@@ -44,12 +52,13 @@ download.json()
 e=requests.post('http://localhost:9300/nifi-api/flowfile-queues/295fc119-0172-1000-3949-54311cdb478e/drop-requests')
 edata=e.json()
 
-# Read Bulletin
+# Read bulletin board data
 b=requests.get('http://localhost:9300/nifi-api/flow/bulletin-board')
 bdata=b.json()
 bdata
 
-# Counters
+
+# Retrieve counters data
 b=requests.get('http://localhost:9300/nifi-api/flow/bulletin-board')
 bdata=b.json()
 bdata
