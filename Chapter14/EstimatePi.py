9cbae638fd3da881402da0d8e4d14f8b7f9161c5
ArulArockiasamy <114662003+ArulArockiasamy@users.noreply.github.com>
2023-04-06 14:31:41 +0200

    Update EstimatePi.py

--- Chapter14/EstimatePi.py
+++ Chapter14/EstimatePi.py
@@ -1,43 +1,33 @@
+# Purpose:
+# This script demonstrates estimating the value of Pi using the Monte Carlo method in PySpark.
+#!/usr/bin/env python
+# coding: utf-8
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[23]:
-
-
+# Import necessary libraries
 import findspark
 findspark.init()
 
-
-# In[26]:
-
-
+# Import the pyspark library and create a Spark session
 import pyspark
 from pyspark.sql import SparkSession
-spark=SparkSession.builder.master("spark://pop-os.localdomain:7077").appName('Pi-Estimation').getOrCreate()
+spark = SparkSession.builder.master("spark://pop-os.localdomain:7077").appName('Pi-Estimation').getOrCreate()
 
-
-# In[21]:
-
-
+# Import the random module
 import random
-NUM_SAMPLES=1
 
+# Define the number of samples
+NUM_SAMPLES = 1
+
+# Define a function that returns True if a random point falls inside a unit circle
 def inside(p):
     x, y = random.random(), random.random()
     return x*x + y*y < 1
 
-count = spark.sparkContext('Pi-Example').parallelize(range(0,NUM_SAMPLES)).filter(inside).count()
+# Estimate the value of Pi using the Monte Carlo method
+count = spark.sparkContext('Pi-Example').parallelize(range(0, NUM_SAMPLES)).filter(inside).count()
 print("Pi is roughly {}".format(4.0 * count / NUM_SAMPLES))
 
-
-# In[27]:
-
-
+# Stop the Spark session
 spark.stop()
-
-
-# In[ ]:
-
-
-
-
